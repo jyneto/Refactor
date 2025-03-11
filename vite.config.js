aa85c3cfadcfd4e04ecef277572569b52037acce
jyneto <j-pm@live.se>
2025-03-11 15:11:48 +0100

    added base and custom 404 routing

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: '/Refactor/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404:  resolve(__dirname, "public/404.html"),
+      }
+    }
+  },
 })
