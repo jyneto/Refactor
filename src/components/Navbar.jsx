f2b256dfba1f2ff31b4d111b25c58a3e5ae5961a
jyneto <j-pm@live.se>
2025-07-19 22:34:53 +0200

    Refactor Navbar and CSS styles, remove commented code from various components, and update project fetching logic in GithubProjects component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,16 +13,11 @@ export default function Navbar() {
     <>
       <header className="navbar">
         <h2 className="logo">J.Toledo</h2>
-
-        {/* Mobile menu button */}
         <button className="toggle-input" onClick={toggleMenu}>
           {isNavOpen ? <X size={30} /> : <Menu size={30} />}
         </button>
-
-        {/* Navigation Menu */}
         <nav className={isNavOpen ? "open" : ""}>
           <ul className={isNavOpen ? "nav-menu open" : "nav-menu"}>
-            {/* Close Button inside Mobile Menu */}
             <button className="close-menu" onClick={closeMenu}>
               <X size={30} color="rgba(255, 128, 0, 0.69)" />
             </button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import './components/Navbar.jsx'
-// import './components/Footer.jsx'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { i } from 'framer-motion/client'
